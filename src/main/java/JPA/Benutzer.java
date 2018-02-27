7415199ed678f0cbabd4cf110d71c9ce57f3da43
meike <meike@DESKTOP-DCA62C2.fritz.box>
2018-02-27 19:45:20 +0100

    copy & paste

--- src/main/java/JPA/Benutzer.java
+++ src/main/java/JPA/Benutzer.java
@@ -5,10 +5,124 @@
  */
 package JPA;
 
-/**
- *
- * @author phili
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
 public class Benutzer {
+    @Id
+    @Column(length=30, nullable=false)
+    public String benutzername ="";
+   
+    @Column(nullable=false, length=50)
+    public String passwortHash ="";
+    
+    @Column(nullable=false, length=50)
+    public String vorname ="";
+    
+    @Column(nullable=false, length=50)
+    public String nachname ="";
+    
+    @Column(nullable=false, length=50)
+    public String strasse ="";
+    
+    @Column(nullable=false, length=50)
+    public long plz =0;
+    
+    @Column(nullable=false, length=50)
+    public String ort ="";
+    
+    @Column(nullable=false, length=50)
+    public String land ="";
+    
+    @Column(nullable=false, length=50)
+    public String mail ="";
+    
+    @Column(nullable=false, length=50)
+    public long telefonnummer=0;
+
+    public String getBenutzername() {
+        return benutzername;
+    }
+
+    public void setBenutzername(String benutzername) {
+        this.benutzername = benutzername;
+    }
+
+    public String getPasswortHash() {
+        return passwortHash;
+    }
+
+    public void setPasswortHash(String passwortHash) {
+        this.passwortHash = passwortHash;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public long getPlz() {
+        return plz;
+    }
+
+    public void setPlz(long plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public long getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(long telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+    
+    
     
 }
