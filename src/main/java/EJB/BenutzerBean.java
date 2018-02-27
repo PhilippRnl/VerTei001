21073303e62093d992ee3f8f246bd337773ee7f9
meike <meike@DESKTOP-DCA62C2.fritz.box>
2018-02-27 19:34:14 +0100

    ich habe comitted

--- src/main/java/EJB/BenutzerBean.java
+++ src/main/java/EJB/BenutzerBean.java
@@ -5,10 +5,43 @@
  */
 package EJB;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 /**
  *
  * @author phili
  */
 public class BenutzerBean {
+    @Id
+    @Column(length=30, nullable=false)
+    public String benutzername ="";
+   
+    @Column(nullable=false, length=50)
+    public String passwortHash ="";
+    
+    @Column(nullable=false, length=50)
+    public String vorname ="";
+    
+    @Column(nullable=false, length=50)
+    public String nachname ="";
+    
+    @Column(nullable=false, length=50)
+    public String strasse ="";
+    
+    @Column(nullable=false, length=50)
+    public long plz =0;
+    
+    @Column(nullable=false, length=50)
+    public String ort ="";
+    
+    @Column(nullable=false, length=50)
+    public String land ="";
+    
+    @Column(nullable=false, length=50)
+    public String mail ="";
+    
+    @Column(nullable=false, length=50)
+    public long telefonnummer=0;
     
 }
